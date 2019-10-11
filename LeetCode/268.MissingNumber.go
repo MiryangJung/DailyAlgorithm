296d7a8a0b9ef04b1063469c812d6e1be756533b
MiryangJung <miryang.dev@gmail.com>
2019-10-11 09:30:25 +0900

    19/10/04

---
+++ LeetCode/268.MissingNumber.go
@@ -0,0 +1,13 @@
+/*
+Runtime: 16 ms
+Memory Usage: 5.9 MB
+*/
+
+func missingNumber(nums []int) int {
+    var arrsum, allsum int
+    for i:=0; i<len(nums); i++{
+        arrsum+=nums[i]
+        allsum+=i
+    }
+    return (allsum+len(nums))-arrsum
+}
